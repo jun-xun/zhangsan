e93990a8a438fc466d3d56977da8325c0613a595
jun-xun <1148368634@qq.com>
2020-05-30 12:19:34 +0800

    第一次向Github提交代码

---
+++ demo001.py
@@ -0,0 +1,37 @@
+"""
+print("Hello World!") #字符串
+print(2333) #整数
+print(2.333) #小数
+print(True) #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典
+
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻") #字符串的拼接（只能同类型）
+print("哈哈"*100)
+print(1+2)
+print(2<3)
+name="张三"
+print(name)
+"""
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+
+#数据格式的转换
+# print(type(298.99))
+# print(type(233))
+# print(type("哈哈哈"))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+a='哈哈哈'
+print(len(a))
+
+a=input("请输入：")
+b=input("请输入：")
+print("两串字符串长度和为：",len(a)+len(b))
